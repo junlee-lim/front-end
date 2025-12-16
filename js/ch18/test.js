84ad589001b3b8c783426324341ebc7158b2b5dc
junlee <myyouthhada@naver.com>
2025-12-16 17:25:09 +0900

    12월 16일

--- js/ch18/test.js
+++ js/ch18/test.js
@@ -1,3 +1,7 @@
 let val = -15;
 const result = (val >= 0 ? val : val*-1);
 console.log('result: ', result);
+
+let val2 = -15;
+const result2 = (val < 0 ? -val : val);
+console.log('result: ', result2);
