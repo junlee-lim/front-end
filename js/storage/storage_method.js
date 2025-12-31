0f1f3956555b517fb7d18ee1e7b8a361b5fe485c
junlee <myyouthhada@naver.com>
2025-12-31 17:25:57 +0900

    12.31 오후

---
+++ js/storage/storage_method.js
@@ -0,0 +1,19 @@
+const arr = [];
+
+for(let i = 1; i<=100; i++){
+    // arr.push({id: i, name: `홍길동${i}`});
+    const obj = {
+        id: i,
+        name: '홍길동' + i
+    }
+    arr.push(obj)
+}
+console.log(arr);
+
+let result = null;
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i].id === 20){result = arr[i]}
+}
+
+console.log(result)
